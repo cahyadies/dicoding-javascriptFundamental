ecbef3aaad716da1c5adf95c2e1675806cce2542
Eko Sugeng Cahyadi <ekosugengcahyadi@gmail.com>
2022-07-12 13:13:26 +0700

    nambahin materi destcruring array swap

--- 2.dataStructure/6.destructuringArray/6.2.destructuringAssignment.js
+++ 2.dataStructure/6.destructuringArray/6.2.destructuringAssignment.js
@@ -1,10 +1,25 @@
-const favorites = ["Seafodd", "Salad", "Nugget", "Soup"]
+const favorites = ["Seafood", "Salad", "Nugget", "Soup"];
 
-let myFood = "Ice Cream"
-let herFood = "Noodles"
+let myFood = "Ice Cream";
+let herFood = "Noodles";
 
-[myFood, herFood] = favorites
+[myFood, herFood] = favorites;
 
 console.log(myFood);
 console.log(herFood);
+
+// Contoh lain destructuring array 
+
+let a = 1;
+let b = 2;
+console.log("\nContoh lain destructuring array ");
+console.log("Sebelum Swap");
+console.log("nilai a:" + a);
+console.log("nilai b:" + b);
+
+[a, b] = [b, a]
+
+console.log("Setelah Swap");
+console.log("nilai a:" + a);
+console.log("nilai b:" + b);
 
